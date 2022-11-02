157f817fb812973e1c18dcaeefd17fc19a3b57fa
northline <17317557314@163.com>
2022-11-02 22:41:33 +0800

    第8章：网关会话鉴权处理

---
+++ api-gateway-core-08/src/main/java/cn/yong/gateway/datasource/connection/DubboConnection.java
@@ -0,0 +1,36 @@
+package cn.yong.gateway.datasource.connection;
+
+import cn.yong.gateway.datasource.Connection;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.bootstrap.DubboBootstrap;
+import org.apache.dubbo.config.utils.ReferenceConfigCache;
+import org.apache.dubbo.rpc.service.GenericService;
+
+/**
+ * @author Allen
+ * @desc RPC Dubbo Connection
+ * @date 2022/10/31
+ */
+public class DubboConnection implements Connection {
+
+    private final GenericService genericService;
+
+    public DubboConnection(ApplicationConfig applicationConfig, RegistryConfig registryConfig, ReferenceConfig<GenericService> reference) {
+        // 连接远程服务
+        DubboBootstrap bootstrap = DubboBootstrap.getInstance();
+        bootstrap.application(applicationConfig).registry(registryConfig).reference(reference).start();
+        // 获取泛化接口
+        ReferenceConfigCache cache = ReferenceConfigCache.getCache();
+        this.genericService = cache.get(reference);
+    }
+
+    /**
+     * Dubbo 泛化调用：<a href="https://dubbo.apache.org/zh/docsv2.7/user/examples/generic-reference/">Dubbo 方法调用</a>
+     */
+    @Override
+    public Object execute(String method, String[] parameterTypes, String[] parameterNames, Object[] args) {
+        return genericService.$invoke(method, parameterTypes, args);
+    }
+}
